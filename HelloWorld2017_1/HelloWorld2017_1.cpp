6dafddc008491e18369a9d8632bfceff580fce0e
Andres Villarreal <avillarrealpouw@gmail.com>
2024-04-26 15:09:25 -0500

    added boost library use

--- HelloWorld2017_1/HelloWorld2017_1.cpp
+++ HelloWorld2017_1/HelloWorld2017_1.cpp
@@ -5,6 +5,8 @@
 #include <boost/lambda/lambda.hpp>
 #include <iterator>
 #include <algorithm>
+#include <boost/regex.hpp>
+#include <string>
 
 int main()
 {
@@ -14,4 +16,14 @@ int main()
 	std::for_each(
 		in(std::cin), in(), std::cout << (_1 * 3) << " ");
 	std::cout << "Hello World!\n";
+	std::string line;
+	boost::regex pat("^Subject: (Re: |Aw: )*(.*)");
+
+	while (std::cin)
+	{
+		std::getline(std::cin, line);
+		boost::smatch matches;
+		if (boost::regex_match(line, matches, pat))
+			std::cout << matches[2] << std::endl;
+	}
 }
